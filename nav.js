55e3a6ff2d783bb4aa9be149d78a8c6f277ffdfa
Joe Hitchens <joe@sleepless.com>
2015-06-05 14:57:15 -0700

    -

--- nav.js
+++ nav.js
@@ -1,52 +1,9 @@
 
-// Copyright 2015  Sleepless Software Inc.  All Rights Reserved
+// Copyright 2012  Sleepless Software Inc.  All Rights Reserved
 
-function Nav(arg) {
+function Nav(data, func) {
 
-	if(!Nav.ready) {
-		// first time Nav() has been called ... do some init stuff
-	
-		// default NOP showFunc
-		Nav.showFunc = function(){}
-
-		// Utility func to convert current query string to JS object
-		Nav.getData = function() {
-			var o = {}
-			var s = document.location.search
-			if(s) {
-				var kv = s.substr(1).split("&")
-				for(var i = 0; i < kv.length; i++) {
-					var aa = kv[i].split("=")
-					o[aa[0]] = decodeURIComponent(aa[1])
-				}
-			}
-			return o
-		}
-
-		// set initial state for the current URL
-		if(history.replaceState !== undefined) {
-			var state = { pageYOffset: 0, data: Nav.getData() }
-			history.replaceState(state, "", document.location.href)
-		}
-
-		// wire in the pop handler
-		// if there was already something there ... oh well, sorry.
-		window.onpopstate = function(evt) {
-			if(evt.state) {
-				Nav.showFunc(evt.state.data)
-				// XXX window.pageYOffset = evt.state.pageYOffset
-			}
-		}
-
-		Nav.ready = true
-	}
-
-	var data = arg
-	var func = Nav.showFunc
-	if(typeof arg === 'function') {
-		func = Nav.showFunc = arg;
-		data = Nav.getData()
-	}
+	//console.log("Nav "+o2j(data));
 
 	// build a URL with query string from current path and the contents of 'data'
 	var qs = ""
@@ -55,16 +12,48 @@ function Nav(arg) {
 	}
 	var url = document.location.pathname + qs
 
-	if(history.pushState === undefined) {
-		// dumb browser - just punt it
-		document.location = url
-		return
+	var state = { pageYOffset: 0, data: data }
+
+	var default_show = function(qd) {
+		$(".nav").hide()					// hide all pages
+		document.body.scrollIntoView();		// jump to top of document
+		var f = window[ "nav_" + qd.act ]
+		if( typeof f === "function" ) {
+			// a global function exists to handle the page; call it
+			f( qd )
+		}
 	}
 
-	var state = { pageYOffset: window.pageYOffset, data: data }
-	history.pushState(state, "", url)
+	if(!Nav.show) {
+		// first time through 
+		Nav.show = func || default_show; //function(){}
+		var doc = document
+		if(history.replaceState !== undefined) {
+			// set state for the current/initial location
+			history.replaceState(state, "", url)
+			// wire in the pop handler
+			window.onpopstate = function(evt) {
+				if(evt.state) {
+					var data = evt.state
+					Nav.show(evt.state.data)
+					// XXX window.pageYOffset = evt.state.pageYOffset
+				}
+			}
+		}
+	}
+	else {
+		if(history.pushState === undefined) {
+			// dumb browser - just punt it
+			document.location = url
+			return
+		}
+		state.pageYOffset = window.pageYOffset
+		history.pushState(state, "", url)
+	}
 
-	Nav.showFunc(data)
+	if(func)
+		Nav.show = func
+	Nav.show(data)
 
 }
 
